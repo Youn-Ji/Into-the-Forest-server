115a9bc399fe572da5668ef7c6949543847e48dc
Youn_Ji <syj9760@gmail.com>
2021-02-11 19:03:29 +0900

    :lock: complete JWT

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,24 +1,15 @@
-import { Controller, Get, Post, Body, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { AuthGuard } from '@nestjs/passport';
 
-import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
-    private authService: AuthService
     ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
-
-  @Post('user')
-  async getProfile() {
-    return this.authService.validateUser();
-  }
-
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,22 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config'
+import * as jwt from 'jsonwebtoken';
 
 @Injectable() 
 export class AuthService {
   constructor (
-    private jwtService: JwtService
+    private jwtService: JwtService,
+    private configService: ConfigService
   ) {}
 
-  async validateUser() {
-    const payload = { key: 'mongmong' };
+  async sign() {
+    const payload = { key: this.configService.get('SECRET_PAYLOAD') };
     return {
       accessToken: this.jwtService.sign(payload)
     }
   }
 
-  async validate(data: any) {
-    if(data.key === 'mongmong') {
-      return { message: 'ok'}
+  async verify(hostId, token) {
+    let accessToken = jwt.verify(token, this.configService.get('SECRET_JWT_TEMP'))
+    if(accessToken === hostId) {
+      return { response: 'ok'}
+    } else {
+      return { error: 'error'}
+    }
+  }
+
+  async verifyGuard(accessToken) {
+    if(accessToken === this.configService.get('SECRET_PAYLOAD')) {
+      return { response: 'ok'}
+    } else {
+      return { error: 'error'}
     }
   }
 }
