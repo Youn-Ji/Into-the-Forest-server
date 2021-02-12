9bb9998b53ed733b0d48be5b20def6b71099a6a2
urusara13 <urusara13@gmail.com>
2021-02-12 13:37:32 +0900

    nope

--- src/healthCheck/healthCheck.controller.ts
+++ src/healthCheck/healthCheck.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get, HttpCode } from '@nestjs/common';
+import { Controller, UseInterceptors, Post, Body, HttpCode, Logger } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express'
+import { FileService } from '../record/file.service'
+
+
 
 @Controller('healthCheck')
 export class HealthCheckController {
-  constructor() {}
+  private logger: Logger = new Logger('healthCheck')
 
-  @Get()
-  @HttpCode(200)
-  get(){
-    return 'ok'
+  constructor(
+    private readonly fileService: FileService
+  ) {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  @HttpCode(201)
+  async image(@Body() reqData: any){
+    await this.fileService.uploadFile(reqData.data, '.jpg')
   }
 }
